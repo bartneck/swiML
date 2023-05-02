8ff071d69873665182b29bfb45afe71763aa1a60
calloc262 <calloc262@gmail.com>
2023-05-02 15:11:35 +1200

    complete xml to Py output
    completed program to output xml into Python text output
    limited to current xsd

--- pythonXMLtest/xmlToPyOutput.py
+++ pythonXMLtest/xmlToPyOutput.py
@@ -16,6 +16,24 @@ ET.dump(tree)'''
 
 root.find('lengthUnit').text
 
+
+
+def to_time(time):
+    '''converts to unit time'''
+    return (f'{time[2:3]}:{time[4:6]}')
+
+def print_author(author):
+    '''returns author string'''
+    return [f'{author[0].text} {author[1].text}',f'{author[2].text}' if len(author) > 2 else '']
+
+def print_length(length,unit):
+    '''return pools length string'''
+    return f'{length.text} {unit.text} pool'
+
+def print_total_length(length,unit):
+    '''return pools length string'''
+    return f'{length.text} {unit.text}'
+
 def print_stroke(stroke):
     '''returns line detailing what stroke is being swum'''
     for  child in stroke:
@@ -38,17 +56,26 @@ def print_rest(rest):
     '''returns line for rest types'''
     for rest_type in rest:
         if rest_type.tag == 'sinceStart':
-            return f'on {rest_type.text}'
+            return f'on {to_time(rest_type.text)}'
         elif rest_type.tag == 'afterStop':
-            return f' take {rest_type.text} rest'
+            return f' take {to_time(rest_type.text)} rest'
         elif rest_type.tag == 'sinceLastRest':
-            return f'{rest_type.text}'
+            return f'{to_time(rest_type.text)}'
         elif rest_type.tag == 'inOut':
             return f'{rest_type.text} in First out'
 
 def print_breath(breath):
     '''returns line for breathing style'''
     return f'Breathing every {breath.text}'
+
+def print_equipment(equipment):
+    '''returns line for equipment'''
+    return (f'with {equipment}')
+
+def print_underwater(under):
+    '''return underwater based on boolean input'''
+    if under:
+        return 'under the water'
 
 def print_continue(child,prefix):
     '''prints continue heading and instructions within 
@@ -57,6 +84,10 @@ def print_continue(child,prefix):
     prefix.append(' | ')
     for instruction in child[1:]:
         print_instuction(instruction,prefix)
+
+def print_description(desc):
+    '''prints contents'''
+    return desc.text
     
 
 def print_repetition(child,prefix):
@@ -85,15 +116,24 @@ def print_instuction(instruction,prefix):
         # instruction group checks
         for child in instruction:
             if child.tag[:6] == 'length':
-                r_strings[0]+= (f'{child.text} {root.find("lengthUnit").text} ')
+                if child.tag[-4:] == 'Time':
+                    r_strings[0]+= (f'swim for {to_time(child.text)}')
+                else:
+                    r_strings[0]+= (f'{child.text} {root.find("lengthUnit").text} ')
             elif child.tag == 'stroke':
                 r_strings[0]+= (print_stroke(child))
+            elif child.tag == 'equipment':
+                r_strings[0]+= (print_equipment(child))
+            elif child.tag == 'underwater':
+                r_strings.append(print_underwater(child))
             elif child.tag == 'intensity':
                 r_strings.append(print_intensity(child))
             elif child.tag == 'rest':
                 r_strings.append(print_rest(child))
             elif child.tag == 'breath':
                 r_strings.append(print_breath(child))
+            elif child.tag == 'instructionDescription':
+                r_strings.append(print_description(child))
 
         #adds buffer for multi indented instructions for easier reading
         if len(prefix) > 1:
@@ -103,16 +143,40 @@ def print_instuction(instruction,prefix):
         for i,string in enumerate(r_strings):
             for pre in prefix[::-1]:
                 if pre == ' | ':
-                    string = pre + string
+                    string = '  '+pre + string
                 else:
-                    if i+1 == len(r_strings) // 2:
-                        string = (f' {pre[1]}x{pre[0]}') + string
+                    if i == (len(r_strings)-1) // 2:
+                        string = (f'{pre[1]}x{pre[0]}') + string
                     else:
-                        string = (f'   {pre[0]}') + string
+                        string = (f'  {pre[0]}') + string
             print(string)
-for child in root:
-    if child.tag == 'instruction':
-        #print all instructions
-        print('new instruction')
-        print_instuction(child,[])
-    
+print()
+for child2 in root:
+    if child2.tag == 'hideIntro':
+        if child2.text == 'true':
+            for child in root:
+                if child.tag == 'instruction':
+                    #print all instructions
+                    print('new instruction')
+                    print_instuction(child,[])
+        else:
+            for child in root:
+                if child.tag == 'title':
+                    print(child.text)
+                elif child.tag == 'author':
+                    for line in print_author(child):
+                        if len(line) > 0:
+                            print(line)
+                elif child.tag == 'programDescription':
+                    print(print_description(child))
+                elif child.tag == 'creationDate':
+                    print(child.text)
+                elif child.tag == 'poolLength':
+                    print(print_length(child,root.find("lengthUnit")))
+                elif child.tag == 'programLength':
+                    print(print_total_length(child,root.find("lengthUnit")))
+                elif child.tag == 'instruction':
+                    #print all instructions
+                    print(' \nnew instruction')
+                    print_instuction(child,[])
+            
