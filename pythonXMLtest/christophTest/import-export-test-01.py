9af860538dab4efd54a591414ce4d018fac5af45
cba56 <cba56@192.168.1.239>
2024-02-23 14:52:19 +1300

    small change to import export test

--- pythonXMLtest/christophTest/import-export-test-01.py
+++ pythonXMLtest/christophTest/import-export-test-01.py
@@ -1,5 +1,3 @@
 import swiML
-
 program=swiML.readXML('JasiMasters2024021301.xml')
-
-swiML.writeXML('JasiMasters2024021301-phyton.xml',program)
+swiML.writeXML('JasiMasters2024021301-python-out.xml',program)
