657d49752895654585011f965d2f9a76d9007291
cba56 <cba56@192.168.1.239>
2024-03-12 13:18:04 +1300

    new program

--- jasiMasters/JasiMasters2024031001.py
+++ jasiMasters/JasiMasters2024031001.py
@@ -6,7 +6,9 @@ warmUp=swiML.Instruction(
     intensity=('startIntensity',('zone','easy')),
 )
 
-warmUpSegment=swiML.SegmentName('WarmUp')
+warmUpSegment=swiML.SegmentName('Warm Up')
+firstSegment=swiML.SegmentName('First set')
+secondSegment=swiML.SegmentName('Second set')
 
 oneSet1=swiML.Instruction(
     length=('lengthAsDistance',50),
@@ -48,8 +50,69 @@ twoSet2=swiML.Instruction(
 twoSet3=swiML.Instruction(
     length=('lengthAsDistance',300),
     stroke=('standardStroke','freestyle'),
+    rest=('afterStop','PT0M45S'),
+    intensity=('startIntensity',('zone','easy')),
+    intensity=('stopIntensity',('zone','max'))
+)
+
+twoSet4=swiML.Instruction(
+    length=('lengthAsDistance',50),
+    stroke=('standardStroke','freestyle'),
     rest=('afterStop','PT0M30S'),
+    intensity=('startIntensity',('zone','max')),
+)
+twoSet5=swiML.Instruction(
+    length=('lengthAsDistance',50),
+    stroke=('kicking',('standardKick','any')),
+    rest=('afterStop','PT0M30S')
+)
+
+threeSet1=swiML.Instruction(
+    length=('lengthAsDistance',50),
+    stroke=('standardStroke','any'),
+    rest=('afterStop','PT0M30S'),
+    equipment=('pullBuoy','pads')
+)
+threeSet2=swiML.Instruction(
+    length=('lengthAsDistance',50),
+    stroke=('standardStroke','freestyle'),
+    rest=('afterStop','PT0M60S'),
+    intensity=('startIntensity',('zone','max')),
+    equipment=('pullBuoy','pads')
+)
+
+threeSet3=swiML.Instruction(
+    length=('lengthAsDistance',100),
+    stroke=('standardStroke','freestyle'),
+    rest=('afterStop','PT0M15S'),
+    equipment=('pullBuoy','pads')
+)
+
+threeSet4=swiML.Instruction(
+    length=('lengthAsDistance',200),
+    stroke=('standardStroke','freestyle'),
+    rest=('afterStop','PT0M30S'),
+    equipment=('pullBuoy','pads')
+)
+
+threeSet5=swiML.Instruction(
+    length=('lengthAsDistance',300),
+    stroke=('standardStroke','freestyle'),
+    rest=('afterStop','PT0M45S'),
+    equipment=('pullBuoy','pads'),
     intensity=('startIntensity',('zone','easy'),'stopIntensity',('zone','max')),
+)
+
+fourSet1=swiML.Instruction(
+    length=('lengthAsDistance',50),
+    stroke=('standardStroke','notFreestyle'),
+    rest=('sinceStart','PT0M65S'),
+)
+
+fourSet=swiML.Repetition(
+    repetitionCount=6,
+    instructions=[fourSet1]
+
 )
 
 simpleProgram=swiML.Program(
@@ -63,16 +126,23 @@ simpleProgram=swiML.Program(
     instructions=[warmUpSegment,
                   warmUp,
                   oneSet,
+                  firstSegment,
                   twoSet1,
                   twoSet2,
-                  twoSet3
-                #   threeSet,
-                #   fourSet,
-                #   fiveSet,
-                #   sixSet,
-                #   sevenSet,
-                #   eightSet,
-                #   warmDown
+                  twoSet3,
+                  twoSet2,
+                  twoSet1,
+                  twoSet4,
+                  twoSet5,
+                  secondSegment,
+                  threeSet1,
+                  threeSet2,
+                  threeSet3,
+                  threeSet4,
+                  threeSet5,
+                  threeSet4,
+                  threeSet3,
+                  fourSet
                   ]
 )
 
