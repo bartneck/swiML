566315b1e8dc59f49012617cc97024a1fd0e570d
cba56 <cba56@192.168.1.239>
2024-03-15 17:43:32 +1300

    new python book example

---
+++ bookExamples/patterns/impossible-squares/impossible-squares-fermat.py
@@ -0,0 +1,84 @@
+from collections import Counter
+import math
+import swiML
+
+def is_sum_of_two_squares(n):
+    # Get the prime factorization of n
+    prime_factorization = Counter()
+    i = 2
+    while i * i <= n:
+        while n % i == 0:
+            prime_factorization[i] += 1
+            n = n // i
+        i += 1
+    if n > 1:
+        prime_factorization[n] += 1
+    
+    # Check that every prime of the form (4k + 3) occurs an 
+    # even number of times
+    for prime, count in prime_factorization.items():
+        if prime % 4 == 3 and count % 2 != 0:
+            return False
+    return True
+
+def is_square_number(num):
+    # Check if the square root of the number is an integer
+    square_root = math.sqrt(num)
+    return square_root.is_integer()
+
+def find_squares(number):
+    i=1
+    impossible_squares=[]
+    while i <= number:
+        if is_square_number(i):
+            impossible_squares.append("backstroke")
+        elif is_sum_of_two_squares(i):
+            impossible_squares.append("breaststroke")
+        else: 
+            impossible_squares.append("freestyle")
+        print(i," ",impossible_squares[i-1])
+        i+=1
+    return impossible_squares
+
+def create_swiML_instructions(my_list):
+    my_instruction_list=[]
+    i=1
+    while i in range(len(my_list)):
+        my_instruction_list.append(swiML.Instruction(
+            length=('lengthAsLaps',i),
+            stroke=('standardStroke',my_list[i-1]),
+            rest=('afterStop','PT0M15S')
+        ))
+        i+=1
+    return my_instruction_list
+
+def write_program(myInstructions):
+    warmUp=swiML.Instruction(
+        length=('lengthAsDistance',400),
+        stroke=('standardStroke','any'),
+        intensity=('startIntensity',('zone','easy')),
+    )
+    
+    myInstructions[:0]=[swiML.SegmentName('WarmUp'),warmUp]
+
+    simpleProgram=swiML.Program(
+        title='Impossible Squares',
+        author=[('firstName','Christoph'),('lastName','Bartneck')],
+        programDescription='Swim regular, slanted and impossible squares.',
+        poolLength='25',
+        creationDate='2024-03-15',
+        lengthUnit='meters',
+        swiMLVersion='2.0',
+        instructions=myInstructions
+    )
+    
+    swiML.writeXML('patterns/impossible-squares/impossible-squares.xml',simpleProgram)
+
+# the maximum number of laps which is equal to the number of instructions given.
+length_program=16
+# find if the numbers up to length_program are impossible squares
+squares_list=find_squares(length_program)
+# create the swiML instructions based on the squares_list
+instruction_list=create_swiML_instructions(squares_list)
+# write the swiML XML program to disk
+write_program(instruction_list)
