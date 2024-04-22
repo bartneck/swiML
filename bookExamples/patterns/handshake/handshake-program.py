91dc2f6fd702066d2f3fb637227605a90addbfd6
cba56 <cba56@192.168.1.239>
2024-04-23 10:09:48 +1200

    handshake program

--- bookExamples/patterns/handshake/handshake-program.py
+++ bookExamples/patterns/handshake/handshake-program.py
@@ -13,20 +13,32 @@ def triangular_sequence(n):
 
 def create_swiML_instructions():
     my_instruction_list=[]
+    my_continue_list=[]
+
     for i in range(0, nr_terms):
         my_instruction_list.append(swiML.Instruction(
-                length=('lengthAsLaps',triangular_numbers[i]),
-                # stroke=('standardStroke',steps[i]),
+                length=('lengthAsLaps',i),
+                stroke=('standardStroke','notFreestyle'),
                 rest=('afterStop','PT0M15S')
         ))
+        my_instruction_list.append(swiML.Instruction(
+                length=('lengthAsLaps',triangular_numbers[i]-i),
+                stroke=('standardStroke','freestyle'),
+                rest=('afterStop','PT0M15S')
+        ))
+        # add instruction to the <continue> element.
+        my_continue_list.append(swiML.Continue(
+            instructions=my_instruction_list
+        ))
         i+=1
-    return my_instruction_list
+        my_instruction_list=[]
+    return my_continue_list
 
 #writing the swiML program to disk
 def write_program():
     # warm up instructions
     warmUp=swiML.Instruction(
-        length=('lengthAsDistance',400),
+        length=('lengthAsDistance',200),
         stroke=('standardStroke','any'),
         intensity=('startIntensity',('zone','easy')),
     )
@@ -39,34 +51,30 @@ def write_program():
     # assembly of the main instructions
     myInstructions=create_swiML_instructions()
     myInstructions[:0]=[swiML.SegmentName('Warm Up'),warmUp]
-    myInstructions.insert(2,swiML.SegmentName('Recaman set'))
+    myInstructions.insert(2,swiML.SegmentName('Triangular set'))
     myInstructions.append(swiML.SegmentName('Warm down'))
     myInstructions.append(warmDown)
     
     # assemble the description of the swimming program
-    description_text="Swim the first "+str(nr_terms)+" terms of the triangular numbers sequence."
+    description_text="Swim the first ",nr_terms," terms of the triangular numbers sequence."
     
     # create the program
     simpleProgram=swiML.Program(
-        title='Traingular Numbers',
+        title='Triangular Numbers',
         author=[('firstName','Christoph'),('lastName','Bartneck')],
         programDescription=description_text,
         poolLength='25',
         creationDate='2024-04-22',
-        lengthUnit='meters',
+        lengthUnit='laps',
         hideIntro=False,
         swiMLVersion='2.1',
         instructions=myInstructions
     )
     # write swiML XML to file
-    swiML.writeXML('patterns/handshake/handshake-program.xml',simpleProgram)
+    swiML.writeXML('handshake-program.xml',simpleProgram)
 
 # define the number of terms
 nr_terms=8
 
-triangular_numbers = triangular_sequence(nr_terms)
-print("Sequence of", nr_terms, "triangular numbers:", triangular_numbers)
-
 # write the swiML program
 write_program()
-
