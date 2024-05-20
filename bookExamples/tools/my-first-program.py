bdd979bca760d1b6c395b3d587313db207a5eae4
cba56 <cba56@192.168.1.239>
2024-05-21 07:23:09 +1200

    continueLength test

---
+++ bookExamples/tools/my-first-program.py
@@ -0,0 +1,31 @@
+import swiML
+myProgram=[]
+
+# simple instruction
+warmUp=swiML.Instruction(
+    length=('lengthAsDistance',100),
+    stroke=('standardStroke','freestyle'),
+    intensity=('startIntensity',('zone','easy')),
+)
+
+myProgram.append(warmUp)
+
+#writing the swiML program to disk
+def write_program():
+    # create the program
+    simpleProgram=swiML.Program(
+        title='My First Python swiML Program',
+        author=[('firstName','Christoph'),('lastName','Bartneck')],
+        programDescription='This is a small program to demonstrate how to create a swiML program in Python.',
+        poolLength='25',
+        creationDate='2024-05-20',
+        lengthUnit='meters',
+        swiMLVersion='2.1',
+        hideIntro=False,
+        instructions=myProgram
+    )
+    # write swiML XML to file
+    swiML.writeXML('my_first_swiML_program.xml',simpleProgram)
+
+# write the swiML program
+write_program()
